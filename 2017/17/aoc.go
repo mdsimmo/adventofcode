e0508317e382a3fa501eb63c28997b749c98467c
mdsimmo <mdsimmo@gmail.com>
2024-12-02 18:50:16 +1100

    Corrected 2017 17

--- 2017/17/aoc.go
+++ 2017/17/aoc.go
@@ -11,28 +11,21 @@ func main() {
 
 func part2() {
 	steps := 337
-	bufferSize := 1
 	position := 0
-	zeroPos1 := 0
-	for i := 1; i <= 5_000_000; i++ {
+	pos1Val := 0
+	for bufferSize := 1; bufferSize <= 50_000_000; bufferSize++ {
 		position = (position + steps) % bufferSize + 1
-		bufferSize++
 		if position == 1 {
-			zeroPos1 = i
+			pos1Val = bufferSize
 		}
 		if position == 0 {
 			panic("Should never happen")
 		}
-		//check := part1(i)
-		//if check != zeroPos1 {
-		//	panic("Mismatch")
-		//}
-		//fmt.Printf("Zero: %d\n", zeroPos1)
 	}
-	fmt.Printf("Zero Pos: %d\n", zeroPos1)
+	fmt.Printf("Zero Pos: %d\n", pos1Val)
 }
 
-func part1(iters int) int {
+func part1(iters int) {
 	steps := 337
 	buffer := []int { 0 }
 	position := 0
@@ -43,11 +36,6 @@ func part1(iters int) int {
 			buffer[j] = buffer[j-1]
 		}
 		buffer[position] = i
-		//fmt.Printf("[%4d] %4d: %+v\n", i, position, buffer)
 	}
-	//fmt.Printf("Last position: %d\n", buffer[(position+1)%len(buffer)])
-	if buffer[0] != 0 {
-		panic("Buffer 0 was not zero")
-	}
-	return buffer[1]
+	fmt.Printf("Last position: %d\n", buffer[(position+1)%len(buffer)])
 }
